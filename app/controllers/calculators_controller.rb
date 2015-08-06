4c2c8f13f55c03d2e05698873b3331a12fd15522
bryantt23 <bryantt23@gmail.com>
2015-08-06 05:22:26 -0700

    it works now. basically the information goes from the view to the controller, sends it model for calculating, instead of going to create page gets displayed by create.js.erb

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,9 +1,30 @@
 class CalculatorsController < ApplicationController
+  #
+  # def create
+  #   # https://rubymonk.com/learning/books/5-metaprogramming-ruby-ascent/chapters/24-eval/lessons/63-eval
+  #   @sum = eval(params[:data])
+  #   # text_field_tag :data in view
+  # end
 
+
+  # def calculate
   def create
-    # https://rubymonk.com/learning/books/5-metaprogramming-ruby-ascent/chapters/24-eval/lessons/63-eval
-    @sum = eval(params[:data])
-    # text_field_tag :data in view
+    op = params[:op_id]
+    a = params[:a].to_f
+    b = params[:b].to_f
+    calc = Calculation.new(a, b)
+
+    case op
+    when '1'
+      @result = calc.add
+    when '2'
+      @result = calc.subtract
+    when '3'
+      @result = calc.multiply
+    else
+      @result = calc.add
+    end
   end
+
 
 end
